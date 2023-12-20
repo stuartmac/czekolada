9dae5756eeb9edec076fc9a1953015658bb2e6ea
Thomas Down <downt@gene.com>
2023-12-20 19:32:08 +0000

    Proxy will fix up paths in "files" responses if proxy path is non-empty.

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -7,7 +7,54 @@ if (!SLIVKA) {
     process.exit(1);
 }
 
+let SLIVKA_PATHPREFIX;
+{
+    const match = /^.*[^\/](\/.*)/.exec(SLIVKA);
+    if (match && match[1]) {
+        SLIVKA_PATHPREFIX = match[1];
+    }
+}
+
+function pathTrim(p) {
+    if (SLIVKA_PATHPREFIX) {
+        return p.replace(new RegExp('^' + SLIVKA_PATHPREFIX), '');
+    } else {
+        return p;
+    }
+}
+
 module.exports = function(app) {
+    app.use(
+        '/api/jobs/*/files',
+        proxy.createProxyMiddleware({
+            target: SLIVKA,
+            changeOrigin: true,
+            prependPath: true,
+            selfHandleResponse: true,
+            onProxyRes: proxy.responseInterceptor(async (resBuffer, proxyResp, req, resp) => {
+                if (proxyResp.statusCode === 200) {
+                    const resText = resBuffer.toString();
+                    try {
+                        const resData = JSON.parse(resText);
+                        const fixedResData = {
+                            ...resData,
+                            'files': resData.files.map((f) => ({
+                                ...f,
+                                '@content': pathTrim(f['@content']),
+                                '@url': pathTrim(f['@url'])
+                            }))
+                        };
+                        return JSON.stringify(fixedResData, null, 2);
+                    } catch (err) {
+                        console.error(err);
+                    }
+                    return resText;
+                } else {
+                    return resBuffer;
+                }
+            })
+        })
+    );
     app.use(
         '/api',
         proxy.createProxyMiddleware({
